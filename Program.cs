af1e3a90034dee69308f2bd5c4d01358c48fa82a
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-02-21 10:54:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kullanici_0_girene_kadar_tek_cift_toplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            Console.ReadLine();
+            int sayi = 0;
+            if (sayi % 2 == 0)
+            {
+                Console.Write("Girilen Sayı Çifttir");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("Girilen Sayı Tektir");
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
